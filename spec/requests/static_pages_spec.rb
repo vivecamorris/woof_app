73ac5017914f34b327e3b30b5ff125b26c4096d6
vivecamorris <viveca.morris@yale.edu>
2014-07-01 10:28:25 -0400

    Added layout and header

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+    it "should have the content 'Woofenpoofs'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Woofenpoofs')
+      expect(page).to have_title('Yale Woofenpoofs')
+    end
+  end
+
+	describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+      expect(page).to have_title('Contact')
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+      expect(page).to have_title('About')
+    end
+  end
+
+end
