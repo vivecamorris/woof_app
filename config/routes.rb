c350e3d5cc62e713ffeeef8f1310f0658fc4086c
Sonja Peterson <sonja.peterson@gmail.com>
2014-07-01 15:52:47 -0400

    added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 WoofApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get "users/new"
+  root 'static_pages#home'
+  match '/about',    to:'static_pages#about',  via: 'get'
+  match '/contact',  to:'static_pages#contact',  via: 'get'
+  match '/signup',   to:'users#new', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
